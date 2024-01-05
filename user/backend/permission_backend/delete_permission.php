daaa30c82c20594a73d9b931baeb4f9412caa170
Nripesh <nripeshparajuli2@gmail.com>
2024-01-05 13:52:53 +0545

    Finalizing changes

--- user/backend/permission_backend/delete_permission.php
+++ user/backend/permission_backend/delete_permission.php
@@ -1,13 +1,14 @@
 <?php
 include '../../backend/config.php';
 include '../access.php';
+session_start();
 check_user_permission($allowed_permission_type, 'permission');
 checkUserPermission($allowed_permission_slug,'permission_delete');
 $per_id=$_GET['id'];
     $sql="DELETE FROM permission WHERE per_id=$per_id";
     $result=mysqli_query($connect,$sql);
     if($result){
-        echo "Deleted  succesfully";
+        $_SESSION['message']= "Deleted  succesfully";
         header('Location:../../frontend/fetch_permission/index.php');
     }
 ?>
